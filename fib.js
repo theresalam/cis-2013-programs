24ed346c92f7316301461e66e90beafdc7217c9a
theresalam <70539406+theresalam@users.noreply.github.com>
2020-11-19 00:15:34 -0600

    Add files via upload

---
+++ fib.js
@@ -0,0 +1,47 @@
+//Simple function used to access elements on the html DOM
+var $ = function (id) 
+{
+    return document.getElementById(id);
+};
+
+// Main program function that accepts user input for the total number of the
+//Fibonacci series to show and the then runs a loop to create the output
+
+var generate = function () 
+{
+	var intCount = parseInt($("total_fib").value);  //gets user input from the DOM
+	if (isNaN(intCount) || intCount < 2 || intCount > 100)
+    {
+        alert("Error! Choose a number between 2 to 100.");
+        $("total_fib").value = ""; //blanks out the DOM input box
+    }
+else
+{
+	//validate user input that intCount is a number and intCount is not less than 2 or intCount not greater than 100
+	
+	var i=0;  //sets the first number in the Fibonacci series to 0
+	var j=1;  //sets the second number in the Fibonacci series to 1
+	var k;    //k is calculated by adding i+j and represents the next Fibonacci number
+	var stringOutput = "0 1 ";//initialize the Fibonacci series output to include the first two numbers
+
+	// write whatever kind of loop you want, given the above info, to generate the appropriate
+	// number of fibonacci numbers and then post it to the html DOM 
+    while (intCount > 2)
+    {
+        k = i+j;
+        stringOutput = stringOutput + k + " "; //this outputs O 1 + the value of k + the next set of numbers based on the loop
+        i = j;
+        j = k;
+        intCount--; //this decrements the counter variable
+    }
+    $("output").value = stringOutput;
+}
+}; 
+// Push solution back to Output through DOM
+
+window.onload = function () 
+{
+    $("total_fib").value = "";
+    $("output").value = "";
+    $("generate").onclick = generate; //runs the generate function when clicked
+}; 
